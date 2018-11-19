eff7bd973c8e508a7800396740ecb01597ccf921
wyawang <wyfire6116@126.com>
2018-11-19 17:03:16 +0800

    nothing

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 public class Hello
 {
+	private final static String HELLO_WORLD = "HelloWorld!!!";
+
 	public static void main(String[] args)
 	{
-		System.out.println( "HelloWorld!!!" );
+		System.out.println( HELLO_WORLD );
 	}
 }
